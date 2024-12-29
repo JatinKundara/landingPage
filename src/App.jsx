f5e26e82c31f4580b0f2f74352a0230fbfea0d08
JatinKundara <jatinkkumar552@gmail.com>
2024-12-29 21:50:13 +0530

    complete this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Navbar, HeroSection, FeatureSection } from './components/Index'
+import { Navbar, HeroSection, FeatureSection, Workflow, Pricing, Testimonials, Footer } from './components/Index'
 
 function App() {
   return (
     <>
-      <Navbar/>
+    <Navbar/>
+    <div className='max-w-7xl mx-auto pt-20 px-6'>
       <HeroSection />
       <FeatureSection />
+      <Workflow />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </div>
     </>
   )
 } 
--- src/components/Index.js
+++ src/components/Index.js
@@ -2,10 +2,16 @@ import Navbar from "./Navbar";
 import HeroSection from "./HeroSection";
 import FeatureSection from "./FeatureSection";
 import Footer from "./Footer";
+import Workflow from "./WorkFlow";
+import Pricing from "./Pricing";
+import Testimonials from "./Testimonials";
 
 export {
   Navbar,
   HeroSection,
   FeatureSection,
-  Footer
+  Footer,
+  Workflow,
+  Pricing,
+  Testimonials
 }
